a668cf2a2d135415e0a2bd172ba6f03a698d9cd6
staszek7 <35852514+staszek7@users.noreply.github.com>
2018-01-26 23:34:28 +0100

    Update SensorID.java

--- src/SensorID.java
+++ src/SensorID.java
@@ -2,7 +2,6 @@ import com.google.gson.annotations.SerializedName;
 
 public class SensorID {
     @SerializedName("currentMeasurements") private DataOutput dataOutput;
-
     public void printCurrentInfo () {
         dataOutput.printer();
     }
